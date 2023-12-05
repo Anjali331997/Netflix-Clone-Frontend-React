baf2d36dfc20106ea21e5f29161095126356d29b
Anjali331997 <anjaliap3march1997@gmail.com>
2023-12-05 12:29:18 +0530

    error in css of banner

--- src/components/Rowpost/Rowpost.jsx
+++ src/components/Rowpost/Rowpost.jsx
@@ -2,15 +2,43 @@ import React from 'react'
 import './Rowpost.css'
 
 function Rowpost() {
-  return (
-    <div className='row'>
-        <h2>Title</h2>
-        <div className="posters">
-            <img 
-            src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+    return (
+        <div className='row'>
+            <h2>Title</h2>
+            <div className="posters">
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+                <img className='film_poster_img'
+                    src="https://m.media-amazon.com/images/I/710LgznyPxL._AC_UF1000,1000_QL80_.jpg" alt="poster" />
+
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Rowpost
