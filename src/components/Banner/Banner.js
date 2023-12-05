59ad9c8a039901bc8a043e5ae2ed08a632936b0e
Anjali331997 <anjaliap3march1997@gmail.com>
2023-12-05 12:36:53 +0530

    banner css error is cleared

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,6 +4,7 @@ import './Banner.css'
 function Banner() {
     return (
         <div className='banner'>
+            
             <div className='content'>
                 <h1 className='title'> Movie Name</h1>
                 <div className='banner_buttons'>
@@ -14,9 +15,10 @@ function Banner() {
                     A criminal mastermind who goes by "The Professor" has a plan to pull off the biggest heist in recorded history -- to print billions of euros in the Royal Mint of Spain. To help him carry out the ambitious plan, he recruits eight people with certain abilities and who have nothing to lose. The group of thieves take hostages to aid in their negotiations with the authorities, who strategize to come up with a way to capture The Professor. As more time elapses, the robbers prepare for a showdown with the police.
                 </div>
                 
-                <div className="fade_botton">
-                </div>
+               
             </div>
+            <div className="fade_bottom">
+                </div>
         </div>
     )
 }
